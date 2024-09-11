62619f689f9fce7d6388f6a7dbe70d3f619ab42d
Kgomotso (Troos) Nacane <Kgomotso@unconventional.capital>
2024-09-11 22:19:51 +0200

    Fix linters

--- script.js
+++ script.js
@@ -1,52 +1,52 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const menuIcon = document.getElementById('menu-icon');
-    const mobileNav = document.querySelector('.mobile-nav');
-    const closeButton = document.querySelector('.close-button');
-    const viewDetailsButtons = document.querySelectorAll('.view-details');
-    const menuListItems = document.querySelectorAll('.menu-list-mobile a');
+  const menuIcon = document.getElementById('menu-icon');
+  const mobileNav = document.querySelector('.mobile-nav');
+  const closeButton = document.querySelector('.close-button');
+  const viewDetailsButtons = document.querySelectorAll('.view-details');
+  const menuListItems = document.querySelectorAll('.menu-list-mobile a');
 
-    menuIcon.addEventListener('click', () => {
-        mobileNav.style.display = 'block';
+  menuIcon.addEventListener('click', () => {
+    mobileNav.style.display = 'block';
+  });
+
+  closeButton.addEventListener('click', () => {
+    mobileNav.style.display = 'none';
+  });
+
+  menuListItems.forEach((item) => {
+    item.addEventListener('click', () => {
+      mobileNav.style.display = 'none';
     });
+  });
 
-    closeButton.addEventListener('click', () => {
-        mobileNav.style.display = 'none';
+  window.addEventListener('click', (event) => {
+    if (event.target === mobileNav) {
+      mobileNav.style.display = 'none';
+    }
+  });
+
+  const galleryPopups = document.querySelectorAll('.gallery-popup');
+  const closeButtons = document.querySelectorAll('.close');
+
+  viewDetailsButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+      const galleryId = button.getAttribute('data-gallery');
+      const galleryPopup = document.getElementById(galleryId);
+      galleryPopup.style.display = 'flex';
     });
+  });
 
-    menuListItems.forEach(item => {
-        item.addEventListener('click', () => {
-            mobileNav.style.display = 'none';
-        });
+  closeButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+      button.parentElement.parentElement.style.display = 'none';
     });
+  });
 
-    window.addEventListener('click', (event) => {
-        if (event.target === mobileNav) {
-            mobileNav.style.display = 'none';
-        }
+  window.onclick = function (event) {
+    galleryPopups.forEach((popup) => {
+      if (event.target === popup) {
+        popup.style.display = 'none';
+      }
     });
-
-    const galleryPopups = document.querySelectorAll('.gallery-popup');
-    const closeButtons = document.querySelectorAll('.close');
-
-    viewDetailsButtons.forEach(button => {
-        button.addEventListener('click', () => {
-            const galleryId = button.getAttribute('data-gallery');
-            const galleryPopup = document.getElementById(galleryId);
-            galleryPopup.style.display = 'flex';
-        });
-    });
-
-    closeButtons.forEach(button => {
-        button.addEventListener('click', () => {
-            button.parentElement.parentElement.style.display = 'none';
-        });
-    });
-
-    window.onclick = function(event) {
-        galleryPopups.forEach(popup => {
-            if (event.target == popup) {
-                popup.style.display = 'none';
-            }
-        });
-    };
+  };
 });
